7f7b8d3f79058b4fc559b5f3e6a65076f179df79
Zack Elia <github@zacharyelia.com>
2020-11-16 20:32:42 -0500

    Update bclm formula

--- Formula/bclm.rb
+++ Formula/bclm.rb
@@ -1,9 +1,9 @@
 class Bclm < Formula
   desc "macOS command-line utility to limit max battery charge"
   homepage "https://github.com/zackelia/bclm"
-  url "https://github.com/zackelia/bclm/releases/download/v0.0.2/bclm.zip"
-  version "0.0.2"
-  sha256 "43710bce05d83c25f557ac1c645008eb6998569a0e87addf8a55a33f524cc5eb"
+  url "https://github.com/zackelia/bclm/releases/download/v0.0.3/bclm.zip"
+  version "0.0.3"
+  sha256 "ad6ea82fb6b208fc5e744925041b25a90ae74da3270c9dd85230352edd8485fc"
   license "https://github.com/zackelia/bclm/blob/master/LICENSE"
 
   def install
